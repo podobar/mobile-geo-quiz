a5431589987a96e428ea4b30f9a04fada41d9c54
Maciej Maciąg <maciejmaciag@onet.pl>
2021-10-24 09:32:01 +0200

    Create basic map module

---
+++ src/Controller/MapController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MapController extends AbstractController
+{
+    /**
+     * @Route("/map", name="app_map_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('map/index.html.twig');
+    }
+}
